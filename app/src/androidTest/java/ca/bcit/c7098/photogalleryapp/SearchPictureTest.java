9985069dea1e3b17cd0b8180561607653caceb05
William Murphy <will.murphy1994@gmail.com>
2018-09-21 14:54:18 -0700

    Modified tests

--- app/src/androidTest/java/ca/bcit/c7098/photogalleryapp/SearchPictureTest.java
+++ app/src/androidTest/java/ca/bcit/c7098/photogalleryapp/SearchPictureTest.java
@@ -1,6 +1,7 @@
 package ca.bcit.c7098.photogalleryapp;
 
 import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
@@ -11,6 +12,7 @@ import android.view.ViewParent;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -27,10 +29,15 @@ import static org.hamcrest.Matchers.allOf;
 public class SearchPictureTest {
 
     @Rule
+    IntentsTestRule<SearchActivity> mIntentsRule = new IntentsTestRule<>(SearchActivity.class);
+
+    @Rule
     public ActivityTestRule<MainActivity> mMainActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Rule
     public ActivityTestRule<SearchActivity> mSearchActivityTestRule = new ActivityTestRule<>(SearchActivity.class);
+
+
 
     @Test
     public void searchPictureTest() {
