cb37a93b8ea8263167d432a2b87cb10a78e1c115
William Murphy <will.murphy1994@gmail.com>
2018-10-06 16:11:35 -0700

    Start & end date EditText widgets now open DatePickerDialogs

--- app/src/main/java/ca/bcit/c7098/photogalleryapp/ui/SearchActivity.java
+++ app/src/main/java/ca/bcit/c7098/photogalleryapp/ui/SearchActivity.java
@@ -1,7 +1,18 @@
 package ca.bcit.c7098.photogalleryapp.ui;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+import android.app.DatePickerDialog;
+import java.text.SimpleDateFormat;
+
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import ca.bcit.c7098.photogalleryapp.R;
 
@@ -12,5 +23,36 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        EditText start = findViewById(R.id.start_date);
+        DateListener startDateListener = new DateListener(start, this.getApplicationContext());
+        EditText end = findViewById(R.id.end_date);
+        DateListener endDateListener = new DateListener(end, this.getApplicationContext());
+    }
+
+    private class DateListener implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
+        private EditText mEditText;
+        private Context mContext;
+        DateListener(EditText editText, Context context) {
+            mEditText = editText;
+            mContext = context;
+            mEditText.setOnClickListener(this);
+        }
+        @Override
+        public void onClick(View v) {
+            Calendar current = Calendar.getInstance();
+            DatePickerDialog datePicker = new DatePickerDialog(v.getContext(), this, current.get(Calendar.YEAR), current.get(Calendar.MONTH), current.get(Calendar.DATE));
+            datePicker.show();
+        }
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            Calendar c = Calendar.getInstance();
+            c.set(year, month, dayOfMonth);
+            Date date = new Date();
+            date.setTime(c.getTimeInMillis());
+            String string = new SimpleDateFormat("EEE, MMM d yyyy", Locale.CANADA).format(date);
+            mEditText.setText(string);
+        }
     }
 }
